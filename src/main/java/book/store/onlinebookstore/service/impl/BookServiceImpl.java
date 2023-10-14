b1243b4e5c059d3ca768ecaf610fd9f540483029
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-10-14 20:48:57 +0300

    created dto and controller

--- src/main/java/book/store/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/book/store/onlinebookstore/service/impl/BookServiceImpl.java
@@ -1,28 +1,41 @@
 package book.store.onlinebookstore.service.impl;
 
+import book.store.onlinebookstore.dto.BookDto;
+import book.store.onlinebookstore.dto.CreateBookRequestDto;
+import book.store.onlinebookstore.exception.EntityNotFoundException;
+import book.store.onlinebookstore.mapper.BookMapper;
 import book.store.onlinebookstore.model.Book;
 import book.store.onlinebookstore.repository.BookRepository;
 import book.store.onlinebookstore.service.BookService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Override
+    public BookDto save(CreateBookRequestDto book) {
+        return bookMapper.toDto(bookRepository.save(bookMapper.toModel(book)));
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto findById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            return bookMapper.toDto(optionalBook.get());
+        }
+        throw new EntityNotFoundException("Entity not found by id " + id);
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
