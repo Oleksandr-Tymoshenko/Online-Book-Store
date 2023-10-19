c04d62331b6eff8e234659b011a87949505cc651
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-10-19 15:29:51 +0300

    changed annotations and added exception handlers

--- src/main/java/book/store/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/book/store/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,15 +1,16 @@
 package book.store.onlinebookstore.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 public record CreateBookRequestDto(
-        @NotNull String title,
-        @NotNull String author,
-        @NotNull @Pattern(regexp = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$") String isbn,
-        @NotNull @Min(0) BigDecimal price,
+        @NotBlank String title,
+        @NotBlank String author,
+        @NotBlank @Pattern(regexp = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$") String isbn,
+        @NotNull @Positive BigDecimal price,
         String description, String coverImage) {
 
 }
